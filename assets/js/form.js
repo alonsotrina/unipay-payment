121c920dc233d6708da94e653b4c26b420e2dda0
alonso trina <alonsotrina22@gmail.com>
2024-11-11 14:10:11 -0300

    subiendo layout del payment unipay

---
+++ assets/js/form.js
@@ -0,0 +1,22 @@
+  // Selecciona todos los radios y el botón
+  const debtLinks = document.querySelectorAll('.debt-link');
+  const radios = document.querySelectorAll('input[name="listGroupRadio"]');
+  const payButton = document.getElementById('payButton');
+  const contentPayment = document.getElementById('payment')
+  const paymentTotal = document.getElementById('payment-total')
+
+  // Agrega un event listener a cada radio
+  radios.forEach(radio => {
+    radio.addEventListener('change', () => {
+
+      const selectedValue = document.querySelector('input[name="listGroupRadio"]:checked').value;
+      console.log('valorrrr',selectedValue);
+
+      paymentTotal.innerHTML = selectedValue;
+
+
+      if (document.querySelector('input[name="listGroupRadio"]:checked')) {
+        contentPayment.classList.remove('disabled-payment');
+      }
+    });
+  });
